5a56885b8779725e1a5c1879ac097f42451d8fbf
Utkarsh Dixit <88888678+utkarsh-dixit-git@users.noreply.github.com>
2022-01-12 21:48:30 +0530

    Script Doc

--- script.js
+++ script.js
@@ -37,14 +37,3 @@ $('.carousel').owlCarousel({
         }
     }
 });
-const togglePassword = document.querySelector("#togglePassword");
-        const password = document.querySelector("#myInput");
-
-        togglePassword.addEventListener("click", function () {
-            // toggle the type attribute
-            const type = password.getAttribute("type") === "password" ? "text" : "password";
-            password.setAttribute("type", type);
-            
-            // toggle the icon
-            this.classList.toggle("bi-eye");
-        });
