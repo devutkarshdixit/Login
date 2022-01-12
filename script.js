9269427729f8a69a86149d881ccd6a7cf5411fdc
Utkarsh Dixit <utkarshdixit2021@gmail.com>
2022-01-12 21:45:46 +0530

    Script Doc

--- script.js
+++ script.js
@@ -37,3 +37,14 @@ $('.carousel').owlCarousel({
         }
     }
 });
+const togglePassword = document.querySelector("#togglePassword");
+        const password = document.querySelector("#myInput");
+
+        togglePassword.addEventListener("click", function () {
+            // toggle the type attribute
+            const type = password.getAttribute("type") === "password" ? "text" : "password";
+            password.setAttribute("type", type);
+            
+            // toggle the icon
+            this.classList.toggle("bi-eye");
+        });
