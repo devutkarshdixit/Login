b195fe9ad9e65f1800e8a19003d06dcca2a07888
Utkarsh Dixit <88888678+utkarsh-dixit-git@users.noreply.github.com>
2022-01-12 11:21:16 +0530

    Carousel update

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function onlyNumberKey(evt) {
         return false;
     return true;
 }
+
 /*JS for Toggle Menu For small screens*/
 var navLinks = document.getElementById("navLinks");
 function showMenu() {
@@ -14,3 +15,25 @@ function showMenu() {
 function hideMenu() {
     navLinks.style.right = "-200px";
 }
+// owl carousel script
+$('.carousel').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: false
+        },
+        600: {
+            items: 2,
+            nav: false
+        },
+        1000: {
+            items: 3,
+            nav: false
+        }
+    }
+});
