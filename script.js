e3503e3ba7ac0280d73e23f0fb7c36ec90b2ad36
Utkarsh Dixit <utkarshdixit2021@gmail.com>
2022-01-12 20:32:33 +0530

    script

--- script.js
+++ script.js
@@ -37,3 +37,11 @@ $('.carousel').owlCarousel({
         }
     }
 });
+function myFunction() {
+  var x = document.getElementById("myInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
